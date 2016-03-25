7f3f6646a6e25bd9850281eddb0611d83e57d41c
MELANIE CHUAYPRADIT <melaniechuaypradit@MELANIEs-MBP.home>
2016-03-25 11:45:06 -0400

    edit pita.js to remove previous coding

--- js/pita.js
+++ js/pita.js
@@ -1,21 +1,5 @@
-// $(function() {
-//   $('#slides').superslides({
-//     hashchange: true
-//   });
-// });
-
-
-
-
 $(document).ready(function() {
 $(".swiper-container").swiper({
-        // pagination: '.swiper-pagination',
-        // slidesPerView: 1,
-        // paginationClickable: true,
-        // spaceBetween: 30,
-        // keyboardControl: true,
-        // nextButton: '.swiper-button-next',
-        // prevButton: '.swiper-button-prev',
 
         pagination: '.swiper-pagination',
         paginationClickable: true,
@@ -28,22 +12,3 @@ $(".swiper-container").swiper({
     });
 
 });
-// $(document).ready(function() {
- 
-//   $(".owl-carousel").owlCarousel({
- 
-//       navigation : false, // Show next and prev buttons
-//       slideSpeed : 200,
-//       paginationSpeed : 400,
-//       singleItem: true
- 
-//       // "singleItem:true" is a shortcut for:
-//       // items : 1, 
-//       // itemsDesktop : false,
-//       // itemsDesktopSmall : false,
-//       // itemsTablet: false,
-//       // itemsMobile : false
- 
-//   });
- 
-// });
